8238fcce26fa61bfe100682e37b7e40280632ea6
Куксенков Андрей Владимирович <Kuksenkov_AV@usoft.ru>
2019-03-06 16:49:05 +0300

    Add project files.

---
+++ SomeProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CoreNameSpace;
+using ClassLibrary;
+
+namespace SomeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Core core = new Core();
+            Class @class = new Class();
+        }
+    }
+}
